7a6b76cccdd6b0e2f46c9f32cb3fb709c4c71b1a
Matheus Magalhães de Alcantara <matheus.de.alcantara@gmail.com>
2016-12-06 20:25:29 -0200

    Add type assertions

--- lib/assert.lisp
+++ lib/assert.lisp
@@ -1,18 +1,21 @@
-(import string)
-
-(defmacro assert! (cnd msg) `(if (! ,cnd) (error! ,msg 0)))
+(import string (.. sub #s))
+(import list (elem traverse))
+(import base (debug!))
 
 (defun format-value (value)
   (pretty (if (and (table? value) (get-idx value "contents"))
             (get-idx value "contents")
             value)))
 
+
+(defmacro assert! (cnd msg) `(if (! ,cnd) (error! ,msg 0)))
+
 (defmacro assert (&assertions)
-  (let ((handle-eq
+  (let* ((handle-eq
           (lambda (tree)
             `(assert!
                (= ,(car tree) ,(cadr tree))
-               ,(string/..
+               ,(..
                   "expected "
                   (format-value (car tree))
                   " and "
@@ -22,17 +25,34 @@
           (lambda (tree)
             `(assert!
                (/= ,(car tree) ,(cadr tree))
-               ,(string/..
+               ,(..
                   "expected "
                   (format-value (car tree))
                   " and "
                   (format-value (cadr tree))
                   " to be unequal"))))
+        (types '("list" "number" "string" "bool" "thread" "function" "table" "userdata"))
+        (type-assertion? (lambda (str) (and 
+                                         (= (sub str (#s str) (#s str)) "?")
+                                         (elem (sub str 1 (- (#s str) 1)) types)
+                                         )))
+        (handle-ty (lambda (ty x)
+                     `(assert!
+                        (= (type ,x) ,ty)
+                        ,(.. "expected "
+                             (format-value x)
+                             " to be a " ty))))
         )
-    (traverse assertions
+    `(progn ,@(traverse assertions
               (lambda (x)
                 (case (symbol->string (car x))
                   ((= "=") (handle-eq (cdr x)))
                   ((= "/=") (handle-neq (cdr x)))
-                  )))))
+                  ((type-assertion?) (let* ((str (symbol->string (car x)))
+                                            (typ (sub str 1 (- (#s str) 1))))
+                                       (handle-ty typ (cadr x))))
+                  (true (progn
+                          (car x)
+                          (debug! x)))
+                  ))))))
 
