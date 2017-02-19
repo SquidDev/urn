90691cce57103919f16f45fce219e953fa44b16d
Matheus Magalhães de Alcantara <matheus.de.alcantara@gmail.com>
2017-02-19 11:41:14 -0300

    Reanimate the assertion and testing library

---
+++ lib/extra/test.lisp
@@ -0,0 +1,39 @@
+(import string)
+(import lua/debug (traceback))
+(import extra/assert (assert!))
+
+(define tests-passed (gensym))
+(define tests-failed (gensym))
+(define tests-total (gensym))
+
+(defmacro it (name &body) `(progn
+  (inc! ,tests-total)
+  (xpcall
+    (lambda ()
+      ,@body
+      (push-cdr! ,tests-passed ,name))
+    (lambda (,'msg)
+      (push-cdr! ,tests-failed (list ,name (traceback ,'msg)))))))
+
+(defmacro describe (name &body)
+  `(let ((,tests-passed '())
+         (,tests-failed '())
+         (,tests-total 0))
+      ,@body
+
+      (print! "Test results: ")
+      (print! (string/format "%d (%.2f%%) out of %d passed, %d (%.2f%%) out of %d failed"
+        (# ,tests-passed) (* 100 (/ (# ,tests-passed) ,tests-total)) ,tests-total
+        (# ,tests-failed) (* 100 (/ (# ,tests-failed) ,tests-total)) ,tests-total))
+
+      (print! (string/format "\027[1;32m- Passed tests:\027[0m (%d)" (# ,tests-passed)))
+      (for-each ,'passed ,tests-passed
+        (print! (string/format "\27[1;32m+\27[0m %s" ,'passed)))
+
+      (unless (nil? ,tests-failed)
+        (print!)
+
+        (print! (string/format "\027[1;31m- Failed tests:\027[0m (%d)" (# ,tests-failed)))
+        (for-each ,'failed ,tests-failed
+          (print! (string/format "\27[1;31m*\27[0m %s" (car ,'failed)))
+          (print! (string/format "\27[1;90m  %s\27[0m" (cadr ,'failed)))))))
