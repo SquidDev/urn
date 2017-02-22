898ce655e6e0ea00e204ff8168d98aa3199c95e2
Matheus Magalhães de Alcantara <matheus.de.alcantara@gmail.com>
2017-02-22 20:11:07 -0300

    [ci skip] document string

--- lib/string.lisp
+++ lib/string.lisp
@@ -6,16 +6,19 @@
 (import lua/string () :export)
 (import lua/table (empty-struct))
 
-
 (defun char-at (xs x)
+  "Index the string XS, returning the character at position X."
   (sub xs x x))
 
 (defun .. (&args)
+  "Concatenate the several values given in ARGS. They must all be strings."
   (concat args))
 
-(define #s len)
+(define #s "Return the length of the string X." len)
 
 (defun split (text pattern limit)
+  "Split the string given by TEXT in at most LIMIT components, which are
+   delineated by the Lua pattern PATTERN."
   (let* [(out '())
          (loop true)
          (start 1)]
@@ -36,6 +39,7 @@
 
 
 (define quoted
+  "Quote the string STR so it is suitable for printing."
   (with (escapes (empty-struct))
     ;; Define some mappings for escape characters
     (set-idx! escapes "\t" "\\9")
