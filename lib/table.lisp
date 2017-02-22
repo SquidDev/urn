ed6b8b53266cad3b0828b473fe6ac1ff8f3f8881
Matheus Magalhães de Alcantara <matheus.de.alcantara@gmail.com>
2017-02-22 20:18:17 -0300

    [ci skip] Document `table`

--- lib/table.lisp
+++ lib/table.lisp
@@ -8,6 +8,8 @@
 (import binders (let))
 
 (defun assoc (list key or-val)
+  "Return the value given by KEY in the association list LIST, or, in
+   the case that it does not exist, the value OR-VAL, which can be nil."
   (cond
     [(or (! (list? list))
          (nil? list))
@@ -17,6 +19,7 @@
     [true (assoc (cdr list) key or-val)]))
 
 (defun assoc? (list key)
+  "Check that KEY is bound in the association list LIST."
   (cond
     [(or (! (list? list))
          (nil? list))
@@ -26,10 +29,13 @@
     [true (assoc? (cdr list) key)]))
 
 (defun insert (list_ key val)
+  "Extend the association list LIST_ by inserting VAL, bound to the key KEY."
   (cons (list key val) list_))
 
-
 (defun assoc->struct (list)
+  "Convert the association list LIST into a structure. Much like [[assoc]],
+   in the case there are several values bound to the same key, the first
+   value is chosen."
   (let [(ret '())]
     (traverse list
       (lambda (x)
@@ -42,6 +48,9 @@
     ret))
 
 (defun struct->assoc (tbl)
+  "Convert the structure TBL into an association list. Note that
+   `(eq? x (struct->assoc (assoc->struct x)))` is not guaranteed,
+   because duplicate elements will be removed."
   (with (out '())
     (iter-pairs tbl (lambda (k v)
                       (push-cdr! out (list k v))))
@@ -49,11 +58,13 @@
 
 ;; Chain a series of index accesses together
 (defmacro .> (x &keys)
+  "Index the structure X with the sequence of accesses given by KEYS."
   (with (res x)
     (for-each key keys (set! res `(get-idx ,res ,key)))
     res))
 
 (defmacro .<! (x &keys value)
+  "Set the value at KEYS in the structure X to VALUE."
   (with (res x)
     (for i 1 (- (# keys) 1) 1
       (with (key (get-idx keys i))
@@ -61,6 +72,19 @@
     `(set-idx! ,res ,(get-idx keys (# keys) 1) ,value)))
 
 (defun struct (&keys)
+  "Return the structure given by the list of pairs XS. Note that, in contrast
+   to variations of [[LET]], the pairs are given \"unpacked\": Instead of invoking
+   ```
+   (struct [(:foo bar)])
+   ```
+   or
+   ```
+   (struct {:foo bar})
+   ```
+   , you must instead invoke it like
+   ```
+   (struct :foo bar)
+   ```."
   (if (= (% (# keys) 1) 1)
     (error "Expected an even number of arguments to struct" 2)
     '())
@@ -74,9 +98,11 @@
     out))
 
 (defun empty-struct? (xs)
+  "Check that XS is the empty struct."
   (! (next xs)))
 
 (defun #keys (st)
+  "Return the number of keys in the structure ST."
   (with (cnt 0)
     (iter-pairs st (lambda () (set! cnt (+ cnt 1))))
     cnt))
