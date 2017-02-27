8431b5cbc71736fc06c4b8c9f18e07e826bb37ed
SquidDev <bonzoweb@hotmail.co.uk>
2017-02-27 23:53:36 +0000

    Mark string methods as constant too

--- lib/lua/utf8.meta.lua
+++ lib/lua/utf8.meta.lua
@@ -1,9 +1,9 @@
 local utf8 = utf8 or {}
 return {
-	["char"] =        { tag = "var", contents = "utf8.char",        value = utf8.char,        },
-	["charpattern"] = { tag = "var", contents = "utf8.charpattern", value = utf8.charpattern, },
-	["codepoint"] =   { tag = "var", contents = "utf8.codepoint",   value = utf8.codepoint,   },
-	["codes"] =       { tag = "var", contents = "utf8.codes",       value = utf8.codes,       },
-	["len"] =         { tag = "var", contents = "utf8.len",         value = utf8.len,         },
-	["offset"] =      { tag = "var", contents = "utf8.offset",      value = utf8.offset,      },
+	["char"] =        { tag = "var", contents = "utf8.char",        value = utf8.char,        pure = true, },
+	["charpattern"] = { tag = "var", contents = "utf8.charpattern", value = utf8.charpattern, pure = true, },
+	["codepoint"] =   { tag = "var", contents = "utf8.codepoint",   value = utf8.codepoint,   pure = true, },
+	["codes"] =       { tag = "var", contents = "utf8.codes",       value = utf8.codes,       pure = true, },
+	["len"] =         { tag = "var", contents = "utf8.len",         value = utf8.len,         pure = true, },
+	["offset"] =      { tag = "var", contents = "utf8.offset",      value = utf8.offset,      pure = true, },
 }
