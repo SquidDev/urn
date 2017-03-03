796322c0494d36fd7cd35695d410912a5d377709
Matheus Magalhães de Alcantara <matheus.de.alcantara@gmail.com>
2017-03-02 21:43:35 -0300

    More conversion.

--- urn/backend/lisp.lisp
+++ urn/backend/lisp.lisp
@@ -4,52 +4,52 @@
 (defun estimate-length (node max)
   (with (tag (.> node :tag))
     (cond
-      ((or (= tag "string") (= tag "number") (= tag "symbol") (= tag "key"))
-        (#s (number->string (.> node :contents))))
-      ((= tag "list")
-        (let* ((sum 2)
+      [(or (= tag "string") (= tag "number") (= tag "symbol") (= tag "key"))
+       (#s (number->string (.> node :contents)))]
+      [(= tag "list")
+       (let* ((sum 2)
               (i 1))
-          (while (and (<= sum max) (<= i (# node)))
-            (set! sum (+ sum (estimate-length (nth node i) (- max sum))))
-            (when (> i 1) (inc! sum))
-            (inc! i))
-          sum))
-      (true (fail! (.. "Unknown tag " tag))))))
+         (while (and (<= sum max) (<= i (# node)))
+                (set! sum (+ sum (estimate-length (nth node i) (- max sum))))
+                (when (> i 1) (inc! sum))
+                (inc! i))
+         sum)]
+      [true (fail! (.. "Unknown tag " tag))])))
 
 (defun expression (node writer)
   (with (tag (.> node :tag))
     (cond
-        ((= tag "string") (writer/append! writer (quoted (.> node :value))))
-        ((= tag "number") (writer/append! writer (number->string (.> node :value))))
-        ((= tag "key") (writer/append! writer (.. ":" (.> node :value))))
-        ((= tag "symbol") (writer/append! writer (.> node :contents)))
-        ((= tag "list")
-          (writer/append! writer "(")
+        [(= tag "string") (writer/append! writer (quoted (.> node :value)))]
+        [(= tag "number") (writer/append! writer (number->string (.> node :value)))]
+        [(= tag "key") (writer/append! writer (.. ":" (.> node :value)))]
+        [(= tag "symbol") (writer/append! writer (.> node :contents))]
+        [(= tag "list")
+         (writer/append! writer "(")
 
-          (if (nil? node)
-            (writer/append! writer ")")
-            (let ((newline false)
-                  (max (- 60 (estimate-length (car node) 60))))
-              (expression (car node) writer)
-              (when (<= max 0)
-                (set! newline true)
-                (writer/indent! writer))
-              (for i 2 (# node) 1
-                (with (entry (nth node i))
-                  (when (and (! newline) (> max 0))
-                    (set! max (- max (estimate-length entry max)))
-                    (when (<= max 0)
-                      (set! newline true)
-                      (writer/indent! writer)))
+         (if (nil? node)
+           (writer/append! writer ")")
+           (let ((newline false)
+                 (max (- 60 (estimate-length (car node) 60))))
+             (expression (car node) writer)
+             (when (<= max 0)
+               (set! newline true)
+               (writer/indent! writer))
+             (for i 2 (# node) 1
+                  (with (entry (nth node i))
+                        (when (and (! newline) (> max 0))
+                          (set! max (- max (estimate-length entry max)))
+                          (when (<= max 0)
+                            (set! newline true)
+                            (writer/indent! writer)))
 
-                  (if newline
-                    (writer/line! writer)
-                    (writer/append! writer " "))
-                  (expression entry writer)))
+                        (if newline
+                          (writer/line! writer)
+                          (writer/append! writer " "))
+                        (expression entry writer)))
 
-              (when newline (writer/unindent! writer))
-              (writer/append! writer ")"))))
-        (true (fail! (.. "Unknown tag " tag))))))
+             (when newline (writer/unindent! writer))
+             (writer/append! writer ")")))]
+        [true (fail! (.. "Unknown tag " tag))])))
 
 (defun block (list writer)
   (for-each node list
