37a96dcdf1565d3660483bf173e4f11ba73606ec
Matheus Magalhães de Alcantara <matheus.de.alcantara@gmail.com>
2017-03-05 15:26:51 -0300

    Document the grammar of patterns

--- lib/match.lisp
+++ lib/match.lisp
@@ -1,3 +1,33 @@
+"A pattern matching library.
+ Utilities for manipulating deeply-nested data and lists in general,
+ as well as binding multiple values.
+
+ The grammar of patterns is described below:
+ ```
+ pattern ::= literal
+           | symbol
+           | _
+           | ( pattern * ) ;; list
+           | ( pattern . pattern ) ;; cons
+ ```
+ 
+ A literal pattern matches only if the scrutinee (what's being matched)
+ compares [[eq?]] to the literal.
+
+ Both symbol patterns and the wildcard, `_`, match anything. However, a
+ symbol will bind the result of matching to that symbol. For example,
+
+ ```
+ (destructuring-bind [x 1]
+   (print! x))
+ ```
+ Results in `1` being printed to standard output, seeing as it is bound to
+ `x`.
+
+ List patterns and cons patterns match lists. A list pattern will match every
+ element in a list, while a cons pattern will only match the car and the cdr.
+ Both bind everything bound by their \"inner\" patterns."
+
 (import base ( defun defmacro if debug print
                let* and gensym error
                quasiquote /= # for
