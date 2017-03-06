d4a248f2cc5304fa256bf20fb79fbffb2890ff6d
Matheus Magalhães de Alcantara <matheus.de.alcantara@gmail.com>
2017-03-06 20:14:14 -0300

    Add docs for define-ffi

--- lib/extra/ffi.lisp
+++ lib/extra/ffi.lisp
@@ -2,5 +2,7 @@
 (import base (const-val type#))
 
 (defmacro defun-ffi (name typedecl)
+  "Define the external symbol NAME with the C type signature
+   given by TYPEDECL."
   (cdef (get-idx typedecl :value))
   `(define ,name (get-idx C ,(symbol->string name))))
