44429b37a59bf647a89399e2643c08fa4ee79c08
SquidDev <bonzoweb@hotmail.co.uk>
2017-03-07 18:38:28 +0000

    Unmangle variable names in error messages

--- tacky/traceback.lua
+++ tacky/traceback.lua
@@ -13,11 +13,13 @@ local _temp = (function()
 	}
 end)()
 for k, v in pairs(_temp) do _libs["lua/basic-0/".. k] = v end
-local _3d_1, _3c_1, _3c3d_1, _3e_1, _2b_1, _2d_1, _25_1, error1, getIdx1, setIdx_21_1, tonumber1, type_23_1, _23_1, format1, gsub1, match1, concat1, emptyStruct1, iterPairs1, list1, list_3f_1, key_3f_1, type1, nth1, _2e2e_1, struct1, succ1, huge1, remapMessage1, remapTraceback1, generateMappings1
+local _3d_1, _2f3d_1, _3c_1, _3c3d_1, _3e_1, _3e3d_1, _2b_1, _2d_1, _25_1, error1, getIdx1, setIdx_21_1, tonumber1, type_23_1, _23_1, char1, find1, format1, gsub1, len1, lower1, match1, sub1, concat1, emptyStruct1, iterPairs1, list1, list_3f_1, key_3f_1, between_3f_1, type1, nth1, pushCdr_21_1, charAt1, _2e2e_1, struct1, succ1, huge1, unmangleIdent1, remapError1, remapMessage1, remapTraceback1, generateMappings1
 _3d_1 = function(v1, v2) return (v1 == v2) end
+_2f3d_1 = function(v1, v2) return (v1 ~= v2) end
 _3c_1 = function(v1, v2) return (v1 < v2) end
 _3c3d_1 = function(v1, v2) return (v1 <= v2) end
 _3e_1 = function(v1, v2) return (v1 > v2) end
+_3e3d_1 = function(v1, v2) return (v1 >= v2) end
 _2b_1 = function(v1, v2) return (v1 + v2) end
 _2d_1 = function(v1, v2) return (v1 - v2) end
 _25_1 = function(v1, v2) return (v1 % v2) end
@@ -29,9 +31,14 @@ type_23_1 = type
 _23_1 = (function(x1)
 	return x1["n"]
 end)
+char1 = string.char
+find1 = string.find
 format1 = string.format
 gsub1 = string.gsub
+len1 = string.len
+lower1 = string.lower
 match1 = string.match
+sub1 = string.sub
 concat1 = table.concat
 emptyStruct1 = function() return {} end
 iterPairs1 = function(x, f) for k, v in pairs(x) do f(k, v) end end
@@ -45,10 +52,18 @@ end)
 key_3f_1 = (function(x3)
 	return (type1(x3) == "key")
 end)
-type1 = (function(val1)
-	local ty1 = type_23_1(val1)
+between_3f_1 = (function(val1, min1, max1)
+	local r_221 = (val1 >= min1)
+	if r_221 then
+		return (val1 <= max1)
+	else
+		return r_221
+	end
+end)
+type1 = (function(val2)
+	local ty1 = type_23_1(val2)
 	if (ty1 == "table") then
-		local tag1 = val1["tag"]
+		local tag1 = val2["tag"]
 		if tag1 then
 			return tag1
 		else
@@ -60,6 +75,20 @@ type1 = (function(val1)
 end)
 nth1 = (function(xs2, idx1)
 	return xs2[idx1]
+end)
+pushCdr_21_1 = (function(xs3, val3)
+	local r_461 = type1(xs3)
+	if (r_461 ~= "list") then
+		error1(format1("bad argment %s (expected %s, got %s)", "xs", "list", r_461), 2)
+	else
+	end
+	local len2 = (_23_1(xs3) + 1)
+	xs3["n"] = len2
+	xs3[len2] = val3
+	return xs3
+end)
+charAt1 = (function(xs4, x4)
+	return sub1(xs4, x4, x4)
 end)
 _2e2e_1 = (function(...)
 	local args1 = _pack(...) args1.tag = "list"
@@ -80,7 +109,7 @@ struct1 = (function(...)
 	r_721 = (function(r_731)
 		if (r_731 <= r_741) then
 			local key2 = keys1[r_731]
-			local val2 = keys1[(1 + r_731)]
+			local val4 = keys1[(1 + r_731)]
 			out1[(function()
 				if key_3f_1(key2) then
 					return contents1(key2)
@@ -88,7 +117,7 @@ struct1 = (function(...)
 					return key2
 				end
 			end)()
-			] = val2
+			] = val4
 			return r_721((r_731 + 2))
 		else
 		end
@@ -96,90 +125,185 @@ struct1 = (function(...)
 	r_721(1)
 	return out1
 end)
-succ1 = (function(x4)
-	return (x4 + 1)
+succ1 = (function(x5)
+	return (x5 + 1)
 end)
 huge1 = math.huge
-remapMessage1 = (function(mappings1, msg1)
-	local r_1441 = list1(match1(msg1, "^(.-):(%d+)(.*)$"))
-	local temp1
-	local r_1461 = list_3f_1(r_1441)
-	if r_1461 then
-		local r_1471 = (_23_1(r_1441) == 3)
-		if r_1471 then
-			temp1 = true
+unmangleIdent1 = (function(ident1)
+	local esc1 = match1(ident1, "^(.-)%d+$")
+	if (esc1 == nil) then
+		return ident1
+	elseif (sub1(esc1, 1, 2) == "_e") then
+		return sub1(ident1, 3)
+	else
+		local buffer1 = {tag = "list", n = 0}
+		local pos1 = 0
+		local len3 = len1(esc1)
+		local r_1611 = nil
+		r_1611 = (function()
+			if (pos1 <= len3) then
+				local char2 = charAt1(esc1, pos1)
+				if (char2 == "_") then
+					local r_1621 = list1(find1(esc1, "^_[%da-z]+_", pos1))
+					local temp1
+					local r_1641 = list_3f_1(r_1621)
+					if r_1641 then
+						local r_1651 = (_23_1(r_1621) == 2)
+						if r_1651 then
+							temp1 = true
+						else
+							temp1 = r_1651
+						end
+					else
+						temp1 = r_1641
+					end
+					if temp1 then
+						local start1 = nth1(r_1621, 1)
+						local _eend1 = nth1(r_1621, 2)
+						pos1 = (pos1 + 1)
+						local r_1681 = nil
+						r_1681 = (function()
+							if (pos1 < _eend1) then
+								pushCdr_21_1(buffer1, char1(tonumber1(sub1(esc1, pos1, succ1(pos1)), 16)))
+								pos1 = (pos1 + 2)
+								return r_1681()
+							else
+							end
+						end)
+						r_1681()
+					else
+						pushCdr_21_1(buffer1, "_")
+					end
+				elseif between_3f_1(char2, "A", "Z") then
+					pushCdr_21_1(buffer1, "-")
+					pushCdr_21_1(buffer1, lower1(char2))
+				else
+					pushCdr_21_1(buffer1, char2)
+				end
+				pos1 = (pos1 + 1)
+				return r_1611()
+			else
+			end
+		end)
+		r_1611()
+		return concat1(buffer1)
+	end
+end)
+remapError1 = (function(msg1)
+	local res1
+	local r_1471
+	local r_1461
+	local r_1451
+	r_1451 = gsub1(msg1, "local '([^']+)'", (function(x6)
+		return _2e2e_1("local '", unmangleIdent1(x6), "'")
+	end))
+	r_1461 = gsub1(r_1451, "global '([^']+)'", (function(x7)
+		return _2e2e_1("global '", unmangleIdent1(x7), "'")
+	end))
+	r_1471 = gsub1(r_1461, "upvalue '([^']+)'", (function(x8)
+		return _2e2e_1("upvalue '", unmangleIdent1(x8), "'")
+	end))
+	res1 = gsub1(r_1471, "function '([^']+)'", (function(x9)
+		return _2e2e_1("function '", unmangleIdent1(x9), "'")
+	end))
+	return res1
+end)
+remapMessage1 = (function(mappings1, msg2)
+	local r_1481 = list1(match1(msg2, "^(.-):(%d+)(.*)$"))
+	local temp2
+	local r_1501 = list_3f_1(r_1481)
+	if r_1501 then
+		local r_1511 = (_23_1(r_1481) == 3)
+		if r_1511 then
+			temp2 = true
 		else
-			temp1 = r_1471
+			temp2 = r_1511
 		end
 	else
-		temp1 = r_1461
+		temp2 = r_1501
 	end
-	if temp1 then
-		local file1 = nth1(r_1441, 1)
-		local line1 = nth1(r_1441, 2)
-		local extra1 = nth1(r_1441, 3)
+	if temp2 then
+		local file1 = nth1(r_1481, 1)
+		local line1 = nth1(r_1481, 2)
+		local extra1 = nth1(r_1481, 3)
 		local mapping1 = mappings1[file1]
 		if mapping1 then
 			local range1 = mapping1[tonumber1(line1)]
 			if range1 then
-				return _2e2e_1(range1, " (", file1, ":", line1, ")", extra1)
+				return _2e2e_1(range1, " (", file1, ":", line1, ")", remapError1(extra1))
 			else
-				return msg1
+				return msg2
 			end
 		else
-			return msg1
+			return msg2
 		end
 	else
-		return msg1
+		return msg2
 	end
 end)
-remapTraceback1 = (function(mappings2, msg2)
-	local r_1521
-	local r_1511
-	r_1511 = gsub1(msg2, "^([^\n:]-:%d+:)", (function(r_1531)
-		return remapMessage1(mappings2, r_1531)
+remapTraceback1 = (function(mappings2, msg3)
+	local r_1601
+	local r_1591
+	local r_1581
+	local r_1571
+	local r_1561
+	local r_1551
+	r_1551 = gsub1(msg3, "^([^\n:]-:%d+:[^\n]*)", (function(r_1671)
+		return remapMessage1(mappings2, r_1671)
 	end))
-	r_1521 = gsub1(r_1511, "\9([^\n:]-:%d+:)", (function(msg3)
-		return _2e2e_1("\9", remapMessage1(mappings2, msg3))
+	r_1561 = gsub1(r_1551, "\9([^\n:]-:%d+:)", (function(msg4)
+		return _2e2e_1("\9", remapMessage1(mappings2, msg4))
 	end))
-	return gsub1(r_1521, "<([^\n:]-:%d+)>", (function(msg4)
-		return _2e2e_1("<", remapMessage1(mappings2, msg4), ">")
+	r_1571 = gsub1(r_1561, "<([^\n:]-:%d+)>\n", (function(msg5)
+		return _2e2e_1("<", remapMessage1(mappings2, msg5), ">\n")
+	end))
+	r_1581 = gsub1(r_1571, "in local '([^']+)'\n", (function(x10)
+		return _2e2e_1("in local '", unmangleIdent1(x10), "'\n")
+	end))
+	r_1591 = gsub1(r_1581, "in global '([^']+)'\n", (function(x11)
+		return _2e2e_1("in global '", unmangleIdent1(x11), "'\n")
+	end))
+	r_1601 = gsub1(r_1591, "in upvalue '([^']+)'\n", (function(x12)
+		return _2e2e_1("in upvalue '", unmangleIdent1(x12), "'\n")
+	end))
+	return gsub1(r_1601, "in function '([^']+)'\n", (function(x13)
+		return _2e2e_1("in function '", unmangleIdent1(x13), "'\n")
 	end))
 end)
 generateMappings1 = (function(lines1)
 	local outLines1 = {}
 	iterPairs1(lines1, (function(line2, ranges1)
 		local rangeLists1 = {}
-		iterPairs1(ranges1, (function(pos1)
-			local file2 = pos1["name"]
+		iterPairs1(ranges1, (function(pos2)
+			local file2 = pos2["name"]
 			local rangeList1 = rangeLists1["file"]
 			if rangeList1 then
 			else
 				rangeList1 = struct1("n", 0, "min", huge1, "max", (0 - huge1))
 				rangeLists1[file2] = rangeList1
 			end
-			local r_1561 = pos1["finish"]["line"]
-			local r_1541 = nil
-			r_1541 = (function(r_1551)
-				if (r_1551 <= r_1561) then
-					if rangeList1[r_1551] then
+			local r_1711 = pos2["finish"]["line"]
+			local r_1691 = nil
+			r_1691 = (function(r_1701)
+				if (r_1701 <= r_1711) then
+					if rangeList1[r_1701] then
 					else
 						rangeList1["n"] = succ1(rangeList1["n"])
-						rangeList1[r_1551] = true
-						if (r_1551 < rangeList1["min"]) then
-							rangeList1["min"] = r_1551
+						rangeList1[r_1701] = true
+						if (r_1701 < rangeList1["min"]) then
+							rangeList1["min"] = r_1701
 						else
 						end
-						if (r_1551 > rangeList1["max"]) then
-							rangeList1["max"] = r_1551
+						if (r_1701 > rangeList1["max"]) then
+							rangeList1["max"] = r_1701
 						else
 						end
 					end
-					return r_1541((r_1551 + 1))
+					return r_1691((r_1701 + 1))
 				else
 				end
 			end)
-			return r_1541(pos1["start"]["line"])
+			return r_1691(pos2["start"]["line"])
 		end))
 		local bestName1 = nil
 		local bestLines1 = nil
