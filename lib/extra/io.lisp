604f3aa257331cc146c4be9ffb053bfbb3c7c1f4
CrazedProgrammer <crazedprogrammer@gmail.com>
2017-03-10 23:08:42 +0100

    Add documentation, improve identation and remove unused parameter

--- lib/extra/io.lisp
+++ lib/extra/io.lisp
@@ -8,53 +8,133 @@
     data))
 
 (defun read-all! (path) 
+  "Reads the data from the file at `path` and returns it as a string.
+   Returns nil if it failed.
+
+   ### Example
+   ```
+   > (read-all! \"hello.txt\")
+   \"Hello, world!\"
+   ```"
   (read-all-mode! path false))
 
 (defun read-lines! (path) 
+  "Reads the lines from the file at `path` and returns it as a list of strings.
+   Returns nil if it failed.
+
+   ### Example
+   ```
+   > (read-lines! \"lines.txt\")
+   (\"This is the first line.\" \"This is the second.\")
+   ```"
   (when-with (data (read-all! path))
     (split data "\n")))
 
 (defun read-bytes! (path)
+  "Reads the data from the file at `path` and returns it as a list of bytes (numbers).
+   Returns nil if it failed.
+
+   ### Example
+   ```
+   > (read-bytes! \"abc.txt\")
+   (97 98 99)
+   ```"
   (when-with (data (read-all-mode! path true))
-    (letrec [(string->bytes (str idx xs)
+    (letrec [(string->bytes (str idx)
                (if (> idx (#s str))
-                 xs
-                 (append xs (append (list (byte str idx))
-                                    (string->bytes str (+ idx 1) `())))))]
+                 `()
+                 (cons (byte str idx)
+                       (string->bytes str (+ idx 1)))))]
       (string->bytes data 1 `()))))
 
 
 (defun write-all-mode! (path append binary data) :hidden
-  (if (when-with (handle (open path (.. (if append "a" "w") (if binary "b" ""))))
-    (self handle :write data)
-    (self handle :close)
-    true
-  ) true false))
+  (with (handle (open path (.. (if append "a" "w") (if binary "b" ""))))
+    (if handle
+      (progn
+        (self handle :write data)
+        (self handle :close)
+        true)
+      false)))
 
 (defun write-all! (path data)
+  "Writes the string `data` to the file at `path`.
+   Creates a new file if it doesn't exist and overwrite the file if it does.
+   Returns true if it succeeded or false if it failed.
+
+   ### Example
+   ```
+   > (write-all! \"hello.txt\" \"Hello, world!\")
+   true
+   ```"
   (write-all-mode! path false false data))
 
 (defun write-lines! (path data)
+  "Writes the lines (list of strings) `data` to the file at `path`.
+   Creates a new file if it doesn't exist and overwrite the file if it does.
+   Returns true if it succeeded or false if it failed.
+
+   ### Example
+   ```
+   > (write-lines! \"lines.txt\" `(\"This is the first line.\" \"This is the second.\"))
+   true
+   ```"
   (write-all! path (concat data "\n")))
 
 (defun write-bytes! (path data)
-  (letrec [(bytes->string (bytes idx xs)
-           (if (> idx (# bytes))
-             xs
-             (append xs (append (list (char (nth bytes idx)))
-                                (bytes->string bytes (+ idx 1) `())))))]
-    (write-all-mode! path false true (concat (bytes->string data 1 `())))))
+  "Writes the bytes (list of numbers) `data` to the file at `path`.
+   Creates a new file if it doesn't exist and overwrite the file if it does.
+   Returns true if it succeeded or false if it failed.
+
+   ### Example
+   ```
+   > (write-bytes! \"abc.txt\" `(97 98 99))
+   true
+   ```"
+  (letrec [(bytes->string (bytes idx)
+             (if (> idx (# bytes))
+               `()
+               (cons (char (nth bytes idx))
+                     (bytes->string bytes (+ idx 1)))))]
+    (write-all-mode! path false true (concat (bytes->string data 1)))))
 
 (defun append-all! (path data)
+  "Appends the string `data` to the file at `path`.
+   Creates a new file if it doesn't exist.
+   Returns true if it succeeded or false if it failed.
+
+   ### Example
+   ```
+   > (append-all! \"hello.txt\" \" Some appended text.\")
+   true
+   ```"
   (write-all-mode! path true false data))
 
 (defun append-lines! (path data)
+  "Appends the lines (list of strings) `data` to the file at `path`.
+   Creates a new file if it doesn't exist.
+   Returns true if it succeeded or false if it failed.
+
+   ### Example
+   ```
+   > (append-lines! \"lines.txt\" `(\" Here's another line:\" \"Another line.\"))
+   true
+   ```"
   (append-all! path (concat data "\n")))
 
 (defun append-bytes! (path data)
-  (letrec [(bytes->string (bytes idx xs)
-           (if (> idx (# bytes))
-             xs
-             (append xs (append (list (char (nth bytes idx)))
-                                (bytes->string bytes (+ idx 1) `())))))]
-    (write-all-mode! path true true (concat (bytes->string data 1 `())))))
+  "Appends the bytes (list of numbers) `data` to the file at `path`.
+   Creates a new file if it doesn't exist.
+   Returns true if it succeeded or false if it failed.
+
+   ### Example
+   ```
+   > (append-bytes! \"abc.txt\" `(100 101 102))
+   true
+   ```"
+  (letrec [(bytes->string (bytes idx)
+             (if (> idx (# bytes))
+               `()
+               (cons (char (nth bytes idx))
+                     (bytes->string bytes (+ idx 1)))))]
+    (write-all-mode! path true true (concat (bytes->string data 1)))))
