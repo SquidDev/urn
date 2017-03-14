8c544ecdd28258b2fcdbd0a027f333c845d60d35
SquidDev <bonzoweb@hotmail.co.uk>
2017-03-14 16:14:49 +0000

    Add node categorising system to the backend
    Instead of simply traversing the tree, this attempts to categorise nodes
    by common patterns, specialising the backend to them instead.

---
+++ urn/backend/lua/escape.lisp
@@ -0,0 +1,61 @@
+(import urn/analysis/nodes (builtins builtin-vars))
+
+(import string)
+
+(defun create-lookup (&lst)
+  "Convert LST to a [[struct]]."
+  :hidden
+  (with (out (empty-struct))
+    (for-each entry lst (.<! out entry true))
+    out))
+
+(define keywords (create-lookup "and" "break" "do" "else" "elseif" "end" "false" "for" "function"
+                   "if" "in" "local" "nil" "not" "or" "repeat" "return" "then" "true"
+                   "until" "while"))
+
+
+(defun escape (name)
+  "Escape an urn identifier NANE, converting it into a form that is valid Lua."
+  (cond
+    [(.> keywords name) (.. "_e" name)] ;; Keywords are trivial to escape
+    [(string/find name "^%w[_%w%d]*$") name] ;; Explicitly forbit leading _ as used for compiler internals
+    [true (let* [(out (if (-> name (string/char-at <> 1) (string/find <> "%d")) "_e" ""))
+                 (upper false)
+                 (esc false)]
+            (for i 1 (string/#s name) 1
+              (with (char (string/char-at name i))
+                (cond
+                  [(and (= char "-")
+                        (-> name (string/char-at <> (pred i)) (string/find <> "[%a%d']"))
+                        (-> name (string/char-at <> (succ i)) (string/find <> "[%a%d']")))
+                   ;; If we're surrounded by ident characters then conver tthe next one to upper case
+                   (set! upper true)]
+                  [(string/find char "[^%w%d]")
+                   (set! char (-> char (string/byte <>) (string/format "%02x" <>)))
+                   (unless esc
+                     (set! esc true)
+                     (set! out (.. out "_")))
+                   (set! out (.. out char))]
+                  [true
+                    (when esc
+                      (set! esc false)
+                      (set! out (.. out "_")))
+                    (when upper
+                      (set! upper false)
+                      (set! char (string/upper char)))
+                    (set! out (.. out char))])))
+            (when esc (set! out (.. out "_")))
+            out)]))
+
+(defun escape-var (var state)
+  "Escape an urn variable, uniquely numbering different variables with the same name."
+  (cond
+    [(.> builtin-vars var) (.> var :name)]
+    [true
+      (let* [(v (escape (.> var :name)))
+             (id (.> state :var-lookup var))]
+        (unless id
+          (set! id (succ (or (.> state :ctr-lookup v) 0)))
+          (.<! state :ctr-lookup v id)
+          (.<! state :var-lookup var id))
+        (.. v (number->string id)))]))
---
+++ urn/backend/lua/init.lisp
@@ -0,0 +1,152 @@
+(import urn/backend/lua/emit ())
+(import urn/backend/lua/escape (escape-var))
+(import urn/backend/writer w)
+(import urn/traceback traceback)
+
+(import extra/assert (assert!))
+(import lua/basic (load))
+(import lua/debug debug)
+(import string)
+
+(defun create-state (meta) (struct
+                             ;; [[run-pass]] options
+                             :level      1
+                             :override   (empty-struct)
+
+                             ;; execute-states options
+                             :count      0
+                             :mappings   (empty-struct)
+
+                             ;; Various lookup tables
+                             :cat-lookup (empty-struct)
+                             :ctr-lookup (empty-struct)
+                             :var-lookup (empty-struct)
+                             :meta       (or meta (empty-struct))))
+
+(defun file (compiler shebang)
+  "Generate a complete file using the current COMPILER state.
+
+   Returns the resulting writer, from which you can extract line mappings
+   and the resulting contents.
+
+   If SHEBANG is specified then it will be prepended."
+  (let* [(state (create-state (.> compiler :libMeta)))
+         (out (w/create))]
+    (when shebang
+      (w/line! out (.. "#!/usr/bin/env " shebang)))
+
+    (.<! state :trace true)
+
+    (prelude out)
+
+    (w/line! out "local _libs = {}")
+
+    ;; Emit all native libraries
+    (for-each lib (.> compiler :libs)
+      (let* [(prefix (string/quoted (.> lib :prefix)))
+             (native (.> lib :native))]
+        (when native
+          (w/line! out "local _temp = (function()")
+          (for-each line (string/split native "\n")
+            (when (/= line "")
+              (w/append! out "\t")
+              (w/line! out line)))
+          (w/line! out "end)()")
+          (w/line! out (.. "for k, v in pairs(_temp) do _libs[" prefix ".. k] = v end")))))
+
+    ;; Count the number of active variables
+    (with (count 0)
+      (for-each node (.> compiler :out)
+        (when-with (var (.> node :defVar))
+          (inc! count)))
+
+      ;; Predeclare all variables. We only do this if we're pretty sure we won't hit the
+      ;; "too many local variable" errors. The upper bound is actually 200, but lambda inlining
+      ;; will probably bring it up slightly.
+      ;; In the future we probably ought to handle this smartly when it is over 150 too.
+      (when (between? count 1 150)
+        (w/append! out "local ")
+        (with (first true)
+          (for-each node (.> compiler :out)
+            (when-with (var (.> node :defVar))
+              (if first
+                (set! first false)
+                (w/append! out ", "))
+              (w/append! out (escape-var var state)))))
+        (w/line! out)))
+
+    (block (.> compiler :out) out state 1 "return ")
+    out))
+
+(defun execute-states (back-state states global logger)
+  "Attempt to execute a series of STATES in the GLOBAL environment.
+
+   BACK-STATE is the backend state, as created with [[create-state]].
+   All errors are logged to LOGGER."
+
+  (let* [(state-list '())
+         (name-list '())
+         (export-list '())
+         (escape-list '())]
+
+    (for i (# states) 1 -1
+      (with (state (nth states i))
+        (unless (= (.> state :stage) "executed")
+          (let* [(node (assert! (.> state :node) (.. "State is in " (.> state :stage) " instead")))
+                 (var (or (.> state :var) (struct :name "temp")))
+                 (escaped (escape-var var back-state))
+                 (name (.> var :name))]
+            (push-cdr! state-list state)
+            (push-cdr! export-list (.. escaped " = " escaped))
+            (push-cdr! name-list name)
+            (push-cdr! escape-list escaped)))))
+
+    (unless (nil? state-list)
+      (let* [(out (w/create))
+             (id (.> back-state :count))
+             (name (concat name-list ","))]
+
+        ;; Update the new count
+        (.<! back-state :count (succ id))
+
+        ;; Setup the function name
+        (when (> 20 (#s name)) (set! name (.. (string/sub name 1 17) "...")))
+        (set! name (.. "compile#" id "{" name "}"))
+
+        (prelude out)
+        (w/line! out (.. "local " (concat escape-list ", ")))
+
+
+        ;; Emit all expressions
+        (for i 1 (# state-list) 1
+          (with (state (nth state-list i))
+            (expression
+              (.> state :node) out back-state
+
+              ;; If we don't have a variable then we'll assign this to the
+              ;; temp variable. Otherwise it will be emitted in the main backend
+              (if (.> state :var)
+                ""
+                (..(nth escape-list i) "= ")))
+            (w/line! out)))
+
+        (w/line! out (.. "return { " (concat export-list ", ") "}"))
+
+        (with (str (w/->string out))
+          ;; Store the traceback
+          (.<! back-state :mappings name (traceback/generate-mappings (.> out :lines)))
+
+          (case (list (load str (.. "=" name) "t" global))
+            [(nil ?msg) (fail! (.. msg ":\n" str))]
+            [(?fun)
+             (case (list (xpcall fun debug/traceback))
+               [(false ?msg)
+                (fail! (traceback/remap-traceback (.> back-state :mappings) msg))]
+               [(true ?tbl)
+                (for i 1 (# state-list) 1
+                  (let* [(state (nth state-list i))
+                         (escaped (nth escape-list i))
+                         (res (.> tbl escaped))]
+                    (self state :executed res)
+                    (when (.> state :var)
+                      (.<! global escaped res))))])]))))))
