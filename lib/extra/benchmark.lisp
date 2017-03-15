d60533bcdfc95b7da875af9d73509c58f244a919
SquidDev <bonzoweb@hotmail.co.uk>
2017-03-15 11:56:22 +0000

    Print number of iterations in benchmark script

--- lib/extra/benchmark.lisp
+++ lib/extra/benchmark.lisp
@@ -4,10 +4,10 @@
   "Modify the function definition BODY so that all executions
    are automatically timed. The time report is printed to standard
    output.
-   
+
    Note that documentation strings and other attributes are
    not preserved, and that the definition must be of the form
-   
+
    ```
    (defun foo (bar) ...)
    ``` "
@@ -23,10 +23,10 @@
   "Modify the function definition BODY so that all executions are
    benchmarked. That is, the function is run repeatededly TIMES times,
    and the average running time is reported on standard output.
-  
+
    Note that documentation strings and other attributes are
    not preserved, and that the definition must be of the form
-   
+
    ```
    (defun foo (bar) ...)
    ``` "
@@ -40,6 +40,7 @@
              (,name ,@args))
            (print! (.. (pretty (list ',name ,@args))
                        " took, on average, "
-                       (/ (- (os/clock) ,time-sym)
-                          ,times)
-                       " seconds per iteration.")))))))
+                       (/ (- (os/clock) ,time-sym) ,times)
+                       " seconds per iteration ("
+                       (- (os/clock) ,time-sym)
+                       " for " ,times " iterations).")))))))
