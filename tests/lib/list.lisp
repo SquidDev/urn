6ae9375a885a7527653c146e9997b7572851a98d
Matheus Magalhães de Alcantara <matheus.de.alcantara@gmail.com>
2017-03-15 20:47:43 -0300

    Switch to property-based testing

--- tests/lib/list.lisp
+++ tests/lib/list.lisp
@@ -1,5 +1,6 @@
 (import extra/test ())
 (import extra/assert ())
+(import extra/check ())
 
 (describe "A quoted list"
   (it "has a constant length"
@@ -8,18 +9,17 @@
               (eq? (# '(foo "foo" 2)) 3)
               (eq? (cadr '(foo "foo")) "foo")))
   (it "compares equal to itself"
-      (affirm (eq? '(1 2 3) '(1 2 3))
-              (eq? '(1 2) '(1 2))))
+      (check [(list a)]
+        (eq? a a)))
   (it "has a congruent operation cdr"
-      (affirm (eq? '(b c d) (cdr '(a b c d)))
-              (eq? (cdr '(a b c)) (cdr '(a b c)))))
+      (check [(list a)]
+        (eq? (drop a 1) (cdr a))))
   (it "has a congruent operation car"
-      (affirm (eq? 'a (car '(a)))
-              (eq? (cdr '(1 2 3)) (cdr '(1 2 3)))))
+      (check [(list a)]
+        (eq? (car a) (nth a 1))))
   (it "can be extended by cons"
-      (affirm (eq? (cons 1 '(2 3)) '(1 2 3))
-              (eq? (cons "foo" '(bar baz)) '("foo" bar baz))
-              (eq? (cons "foo" (list 1 nil 2)) (list "foo" 1 nil 2))))
+      (check [(list a) (any b)]
+        (eq? (cons b a) `(,b ,@a))))
   (it "can be extended by snoc"
       (affirm (eq? (snoc '(1 2) 3) '(1 2 3))
               (eq? (snoc '(bar baz) "foo") '(bar baz "foo"))
@@ -28,14 +28,13 @@
       (affirm (eq? (foldr + 0 '(1 2 3)) (+ (+ 1 2) (+ 3 0)))
               (eq? (foldr append '() '((1 2) (3 4))) '(1 2 3 4))))
   (it "can be appended to another quoted list"
-      (affirm (eq? (append '(1 2) '(3 4)) '(1 2 3 4))
-              (eq? (# (append '(1 2) '(3 4))) (# '(1 2 3 4)))))
+      (check [(list a) (list b)]
+        (eq? (# (append a b)) (+ (# a) (# b)))
+        (eq? (append a b) (reverse (append (reverse b) (reverse a))))))
   (it "can be mapped over"
-      (affirm (eq? (map (cut + <> 1) '(0 1 2)) '(1 2 3))
-              (eq? (map (const 1) '(1 2 3)) '(1 1 1))))
-  (it "remains the same when mapped over with id"
-      (affirm (eq? (map id '(1 2 3)) '(1 2 3))
-              (eq? (traverse '(1 2 3) id) '(1 2 3))))
+      (check [(list a)]
+        (eq? (map id a) a)
+        (eq? (# (map id a)) (# a))))
   (it "has two equivalent operations: map and traverse"
       (affirm (eq? (map (cut + <> 1) '(1 2 3))
                  (traverse '(1 2 3) (cut + <> 1)))))
@@ -46,8 +45,8 @@
   (it "can be built using range"
       (affirm (eq? (range 1 3) '(1 2 3))))
   (it "has a cancellative operation reverse"
-      (affirm (eq? (reverse (reverse '(1 2 3))) '(1 2 3))
-              (eq? (reverse '(3 2 1)) '(1 2 3))))
+      (check [(list a)]
+        (eq? (reverse (reverse a)) a)))
   (it "can be tested for one element matching a predicate"
       (affirm (eq? true (any (cut = <> 1) (range 1 3)))
               (eq? false (any (cut = <> 6) (range 1 3)))))
@@ -69,11 +68,9 @@
       (affirm (eq? (zip list '(1 foo "baz") '(2 bar "qux")) '((1 2) (foo bar) ("baz" "qux")))
               (eq? (zip + '(1 2 3) '(3 7 9)) '(4 9 12))))
   (it "exists"
-      (affirm (eq? true (exists? '(1 2 3)))
-              (eq? true (exists? (range 1 3)))
-              (eq? true (exists? (cons 1 '())))))
+      (check [(list a)] (exists? a)))
   (it "can be accumulated with a monoid"
       (affirm (eq? 10 (accumulate-with tonumber + 0 '(1 2 3 4)))))
   (it "can have an item mutated in-place"
-      (affirm (eq? `(1 3) (with (l `(1 2)) (set-idx! l 2 3) l))))
+      (affirm (eq? '(1 3) (with (l '(1 2)) (set-idx! l 2 3) l))))
 )
