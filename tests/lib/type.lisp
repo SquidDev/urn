6859e1ebb63fde2a7384c5fa99fa24dfaae13f7a
Matheus Magalhães de Alcantara <matheus.de.alcantara@gmail.com>
2017-04-05 15:00:36 -0300

    Convert uses of assert to affirm

--- tests/lib/type.lisp
+++ tests/lib/type.lisp
@@ -3,50 +3,50 @@
 
 (describe "A value"
   (it "may or may not exist"
-      (assert (= true (exists? 1))
+      (affirm (= true (exists? 1))
               (= false (exists? nil))))
   [may "be a number"
        (will "exist"
-             (assert (= true (exists? 1))
+             (affirm (= true (exists? 1))
                      (= true (exists? 2))
                      (= true (exists? (^ 2 40)))))
        (will-not "be a list"
-                 (assert (= false (list? 1))))
+                 (affirm (= false (list? 1))))
        (will-not "be a string"
-                 (assert (= false (string? 1))))]
+                 (affirm (= false (string? 1))))]
   [may "be a table"
        (may "be a list"
             (will "satisfy list?"
-                  (assert (list? '(1 2 3 4))))
+                  (affirm (list? '(1 2 3 4))))
             (will-not "be a number"
-                      (assert (= false
+                      (affirm (= false
                                  (number? '(1 2 3)))))
             (will-not "be a string"
-                      (assert (= false
+                      (affirm (= false
                                  (string? '(1 2 3))))))
        (may "be a symbol"
             (will "satisfy symbol?"
-                  (assert (symbol? 'sym)))
+                  (affirm (symbol? 'sym)))
             (will-not "be a number"
-                      (assert (= false (number? 'sym)))))
+                      (affirm (= false (number? 'sym)))))
        (may "be a (quoted) key"
             (will "satisfy key?"
-                  (assert (key? ':key)))
+                  (affirm (key? ':key)))
             (will-not "be a symbol"
-                      (assert (= false (symbol? ':key)))))]
+                      (affirm (= false (symbol? ':key)))))]
   [may "be a string"
        (will "satisfy string?"
-             (assert (string? "foo")
+             (affirm (string? "foo")
                      (string? "bar")))
        (will-not "be a number"
-                 (assert (= false (number? "foo"))))]
+                 (affirm (= false (number? "foo"))))]
   [may "not exist"
        (will-not "be a number"
-                 (assert (= false (number? nil))))
+                 (affirm (= false (number? nil))))
        (will-not "be a string"
-                 (assert (= false (string? nil))))
+                 (affirm (= false (string? nil))))
        (will "be nil"
-             (assert (= nil nil)))]
+             (affirm (= nil nil)))]
 
   [may "be equated"
        (it "with constants"
