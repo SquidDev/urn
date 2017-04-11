cd7cf42032b42f7f1766569f59909e0a883e4287
Matheus Magalhães de Alcantara <matheus.de.alcantara@gmail.com>
2017-04-11 18:31:43 -0300

    fucking style standards

--- urn/backend/markdown.lisp
+++ urn/backend/markdown.lisp
@@ -24,52 +24,52 @@
   :hidden
   (with (ty (type var))
     (cond
-      ((= ty "builtin")
-        "Builtin term")
-      ((= ty "macro")
-        (.. "Macro defined at " (format-range (get-source (.> var :node)))))
-      ((= ty "native")
-        (.. "Native defined at " (format-range (get-source (.> var :node)))))
-      ((= ty "defined")
-        (.. "Defined at " (format-range (get-source (.> var :node))))))))
+      [(= ty "builtin")
+       "Builtin term"]
+      [(= ty "macro")
+       (.. "Macro defined at " (format-range (get-source (.> var :node))))]
+      [(= ty "native")
+       (.. "Native defined at " (format-range (get-source (.> var :node))))]
+      [(= ty "defined")
+       (.. "Defined at " (format-range (get-source (.> var :node))))])))
 
 (defun format-signature (name var)
   "Attempt to extract the function signature from VAR"
   :hidden
   (with (sig (doc/extract-signature var))
     (cond
-      ((= sig nil) name)
-      ((empty? sig)
-        (.. "(" name ")"))
-      (true
-        (.. "(" name " " (concat (traverse sig (cut get-idx <> "contents")) " ") ")")))))
+      [(= sig nil) name]
+      [(empty? sig)
+       (.. "(" name ")")]
+      [true
+        (.. "(" name " " (concat (map (cut get-idx <> "contents") sig) " ") ")")])))
 
 (defun write-docstring (out str scope)
   (for-each tok (doc/parse-docstring str)
     (with (ty (type tok))
       (cond
-        ((= ty "text") (writer/append! out (.> tok :contents)))
-        ((= ty "boldic") (writer/append! out (.> tok :contents)))
-        ((= ty "bold") (writer/append! out (.> tok :contents)))
-        ((= ty "italic") (writer/append! out (.> tok :contents)))
-        ((= ty "arg")  (writer/append! out (.. "`" (.> tok :contents) "`")))
-        ((= ty "mono") (writer/append! out (.> tok :whole)))
-        ((= ty "link")
-          (let* [(name (.> tok :contents))
-                 (ovar ((.> Scope :get) scope name))]
-            (if (and ovar (.> ovar :node))
-              (let* [(loc (-> (.> ovar :node)
-                          get-source
-                          (.> <> :name)
-                          (string/gsub <> "%.lisp$" "")
-                          (string/gsub <> "/" ".")))
-                     (sig (doc/extract-signature ovar))
-                     (hash (cond
-                             ((= sig nil) (.> ovar :name))
-                             ((empty? sig) (.> ovar :name))
-                             (true (.. name " " (concat (traverse sig (cut get-idx <> "contents")) " ")))))]
-                (writer/append! out (string/format "[`%s`](%s.md#%s)" name loc (string/gsub hash "%A+" "-"))))
-              (writer/append! out (string/format "`%s`" name))))))))
+        [(= ty "text") (writer/append! out (.> tok :contents))]
+        [(= ty "boldic") (writer/append! out (.> tok :contents))]
+        [(= ty "bold") (writer/append! out (.> tok :contents))]
+        [(= ty "italic") (writer/append! out (.> tok :contents))]
+        [(= ty "arg")  (writer/append! out (.. "`" (.> tok :contents) "`"))]
+        [(= ty "mono") (writer/append! out (.> tok :whole))]
+        [(= ty "link")
+         (let* [(name (.> tok :contents))
+                (ovar ((.> Scope :get) scope name))]
+           (if (and ovar (.> ovar :node))
+             (let* [(loc (-> (.> ovar :node)
+                             get-source
+                             (.> <> :name)
+                             (string/gsub <> "%.lisp$" "")
+                             (string/gsub <> "/" ".")))
+                    (sig (doc/extract-signature ovar))
+                    (hash (cond
+                            [(= sig nil) (.> ovar :name)]
+                            [(empty? sig) (.> ovar :name)]
+                            [true (.. name " " (concat (map (cut get-idx <> "contents") sig) " "))]))]
+               (writer/append! out (string/format "[`%s`](%s.md#%s)" name loc (string/gsub hash "%A+" "-"))))
+             (writer/append! out (string/format "`%s`" name))))])))
   (writer/line! out))
 
 (defun exported (out title primary vars scope)
