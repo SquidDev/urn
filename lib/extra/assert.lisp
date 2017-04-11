f0ebdc325a337178a49da4832205208fcf5205f6
Matheus Magalhães de Alcantara <matheus.de.alcantara@gmail.com>
2017-04-11 18:31:33 -0300

    remove assert

--- lib/extra/assert.lisp
+++ lib/extra/assert.lisp
@@ -13,72 +13,6 @@
   "Assert CND is true, otherwise failing with MSG"
   `(unless ,cnd (error! ,msg 0)))
 
-(defmacro assert (&assertions)
-  "Assert each assertion in ASSERTIONS is true
-
-   Each assertion can take several forms:
-
-    - `(= a b)`: Assert that A and B are equal, printing their values if
-      not
-    - `(/= a b)`: Assert that A and B are not equal, printing their
-      values if they are
-    - Type assertions of the form `(list? a)`: Assert that A is of the
-      required type.
-
-   ### Example
-   ```
-   > (assert (= (+ 2 3) 4))
-   [ERROR] expected (+ 2 3) and 4 to be equal
-   > (assert (/= (+ 2 3) 5))
-   [ERROR] expected (+ 2 3) and 5 to be unequal
-   > (assert (string? 2))
-   [ERROR] expected 2 to be a string
-   ```"
-  (let* [(handle-eq
-          (lambda (tree)
-            `(assert!
-               (eq? ,(car tree) ,(cadr tree))
-               ,(..
-                  "expected "
-                  (format-value (car tree))
-                  " and "
-                  (format-value (cadr tree))
-                  " to be equal"))))
-        (handle-neq
-          (lambda (tree)
-            `(assert!
-               (neq? ,(car tree) ,(cadr tree))
-               ,(..
-                  "expected "
-                  (format-value (car tree))
-                  " and "
-                  (format-value (cadr tree))
-                  " to be unequal"))))
-        (types '("list" "number" "string" "bool" "thread" "function" "table" "userdata"
-                 "symbol" "key"))
-        (type-assertion? (lambda (str) (and
-                                         (= (sub str (#s str) (#s str)) "?")
-                                         (elem? (sub str 1 (- (#s str) 1)) types)
-                                         )))
-        (handle-ty (lambda (ty x)
-                     `(assert!
-                        (= (type ,x) ,ty)
-                        ,(.. "expected "
-                             (format-value x)
-                             " to be a " ty))))]
-    `(progn
-       ,@(traverse assertions
-           (lambda (assertion)
-             (with (x (car assertion))
-               (cond
-                 [(eq? '= x) (handle-eq (cdr assertion))]
-                 [(eq? '/= x) (handle-neq (cdr assertion))]
-                 [(type-assertion? (symbol->string x))
-                 (let* [(str (symbol->string x))
-                   (typ (sub str 1 (- (#s str) 1)))]
-                   (handle-ty typ (cadr assertion)))]
-                 [true (print! (pretty assertion))])))))))
-
 (defmacro affirm (&asserts)
   "Assert each expression in ASSERTS evaluates to true
 
@@ -97,7 +31,7 @@
       5
    ```"
   `(progn
-     ,@(traverse asserts
+     ,@( map
          (lambda (expr)
            (let [(bindings '())
                  (vars '())
@@ -125,12 +59,13 @@
              ;; Emit each value and their lines
              (for i (# expr) 1 -1
                (when (nth emit i)
-                 (with (buffer '("\n "))
-                   (for j 1 (pred i) 1
-                     (push-cdr! buffer (if (nth emit j) "|" " "))
-                     (push-cdr! buffer (rep " " (nth lens j))))
-                   (push-cdr! out (concat buffer))
-                   (push-cdr! out `(pretty ,(nth vars i))))))
+                  (with (buffer '("\n "))
+                    (for j 1 (pred i) 1
+                      (push-cdr! buffer (if (nth emit j) "|" " "))
+                      (push-cdr! buffer (rep " " (nth lens j))))
+                    (push-cdr! out (concat buffer))
+                    (push-cdr! out `(pretty ,(nth vars i))))))
              `(let (,@bindings)
                 (unless ,vars
-                  (error! (.. ,@out)))))))))
+                  (error! (.. ,@out))))))
+         asserts )))
