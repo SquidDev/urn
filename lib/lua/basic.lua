e5dfa850e9574c79851bf060c413b80678ab7a97
SquidDev <bonzoweb@hotmail.co.uk>
2017-06-08 08:39:51 +0000

    Fix slice returning lists with negative sizes

--- lib/lua/basic.lua
+++ lib/lua/basic.lua
@@ -2,6 +2,8 @@ return {
 	['slice'] = function(xs, start, finish)
 		if not finish then finish = xs.n end
 		if not finish then finish = #xs end
-		return { tag = "list", n = finish - start + 1, table.unpack(xs, start, finish) }
+		local len = finish - start + 1
+		if len < 0 then len = 0 end
+		return { tag = "list", n = len, table.unpack(xs, start, finish) }
 	end,
 }
