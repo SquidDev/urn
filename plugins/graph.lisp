86402b7c62f201687bd959a9ac947b9dbc3768d2
SquidDev <bonzoweb@hotmail.co.uk>
2017-08-19 17:37:56 +0000

    Add a primitive graph library

---
+++ plugins/graph.lisp
@@ -0,0 +1,32 @@
+(import compiler/nodes compiler)
+(import compiler/pass ())
+(import compiler/resolve compiler)
+
+(import extra/io io)
+(import urn/graph ())
+
+(defmethod (pretty defined) (var) (.> var :full-name))
+(defmethod (pretty native)  (var) (.> var :full-name))
+(defmethod (pretty builtin) (var) (.> var :full-name))
+(defmethod (pretty macro)   (var) (.> var :full-name))
+
+(defpass generate-dependency-graph (state nodes)
+  "Emit an out.dot file, tracking the dependencies between functions."
+  :cat '("warn")
+  (with (graph (make-graph))
+    (for-each node nodes
+      (when-with (var (.> node :def-var))
+        (add-vertex! graph var)))
+
+    (for-each node nodes
+      (when-with (var (.> node :def-var))
+        (with (from (get-vertex graph var))
+          (compiler/visit-node node
+            (lambda (x)
+              (when (symbol? x)
+                (when-with (to (get-vertex graph (.> x :var)))
+                  (add-edge! from to))))))))
+
+    (io/write-all! "out.dot" (graph->dot graph))))
+
+,(add-pass! generate-dependency-graph)
