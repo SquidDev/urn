56c7bd25591861dbce3b75faf28ce8384bbd7ab9
hydraz <urn@semi.works>
2017-10-16 22:52:09 -0200

    Iterators, part 2 (of k): `(map)` and `(range)`.
    I'm quite tired, so I'm doing this in parts.
    
    Part 2 brings:
      · `(map f iter)`. As discussed in the design issue, this returns not a
        particular collection, but rather an iterator object. For lack of a
        better option, we print these as opaque, using “standard” notation.
      · `(range ...)`. This function *also* returns an iteration structure,
        except this one can be printed sanely (because we keep alive all
        relevant information).
    
        The API is the same as in `core/list`, and so are the semantics,
        including the corner cases, e.g. `(range :from 10 :to 1)` won't work
        - you'll have to `(range :from 10 :to 1 :by 9)`.

--- lib/data/iterators.lisp
+++ lib/data/iterators.lisp
@@ -3,6 +3,7 @@
    the updated iterator state, or `nil` twice if no more values can be
    produced.")
 
+;;; Anamorphisms (unfolds) {{{
 
 ;; NOTE: The implementation of iterators for lists and strings alike is
 ;; *fake*. Instead of returning the updated list, for performance
@@ -11,7 +12,8 @@
 ;; These views are not to be manipulated by user code, and should only
 ;; occur during iteration. Because of this, they do not have a `:n` such
 ;; as `list` does.
-(defmethod (next list) (x)
+
+(defmethod (next list) (x) ; {{{
   (if (empty? x)
     nil
     (values-list (car x)
@@ -19,9 +21,9 @@
                    :kth nth
                    :backing x
                    :current 2
-                   :limit (n x) })))
+                   :limit (n x) }))) ; }}}
 
-(defmethod (next string) (x)
+(defmethod (next string) (x) ; {{{
   (if (empty? x)
     nil
     (values-list (string/char-at x 1)
@@ -29,9 +31,9 @@
                    :kth string/char-at
                    :backing x
                    :current 2
-                   :limit (n x) })))
+                   :limit (n x) }))) ; }}}
 
-(defmethod (next view) (x)
+(defmethod (next view) (x) ; {{{
   (destructuring-bind [{ :backing ?backing
                          :current ?current
                          :kth ?kth
@@ -43,9 +45,56 @@
                      :kth kth
                      :backing backing
                      :current (+ 1 current)
-                     :limit lim }))))
+                     :limit lim })))) ; }}}
 
-(defmacro do* (vars &body)
+(defun range (&args) ; {{{
+  "Build an inclusive range, starting at :START and ending at :END, optionally
+   passing by the value :BY."
+  (let* [(ags (apply fast-struct args))
+         (st (or (.> ags :from) 1))
+         (ed (or (.> ags :to) math/huge))
+         (inc (- (or (.> ags :by) (+ 1 st)) st))]
+    { :tag "inclusive-range" :cursor st :end ed :inc inc :tst (if (>= st ed) >= <=) })) ; }}}
+
+(defmethod (next inclusive-range) (range) ; {{{
+  (destructuring-bind [{ :cursor ?c
+                         :end ?e
+                         :inc ?i
+                         :tst ?t}
+                       range]
+    (if (t c e)
+      (values-list c { :tag "inclusive-range" :cursor (+ c i) :end e :inc i :tst t })
+      nil))) ; }}}
+
+(defmethod (pretty inclusive-range) (range) ; {{{
+  (format nil "(range :from {} :to {}{})"
+          (.> range :cursor)
+          (- (.> range :end) 1)
+          (if (/= (.> range :inc) 1)
+            (.. " :by " (+ (.> range :cursor) (.> range :inc)))
+            ""))) ; }}}
+
+;;; }}}
+
+;;; Transformations {{{
+
+(defun map (fun iter) ; {{{
+  "Return an iterator that applies FUN to every non-nil value produced
+   by ITER."
+  { :tag "iterator.map" :fun fun :backing iter }) ; }}}
+
+(defmethod (next iterator.map) (x) ; {{{
+  (destructuring-bind [{ :fun ?fun
+                         :backing ?iter }
+                       x]
+    (let* [((val iter) (next iter))]
+      (if (not iter)
+        nil
+        (values-list (fun val) { :tag "iterator.map" :fun fun :backing iter })))))
+
+(defmethod (pretty iterator.map) (x) "«map iterator»") ; }}}
+
+(defmacro do* (vars &body) ; {{{
   "Run BODY through all elements produced by the iterators in VARS. Note
    that this acts like the cross product instead of a zip."
   (if (empty? vars)
@@ -60,25 +109,42 @@
            (when (and ,val (/= ,val ,marker))
              (let* [(,(caar vars) ,val)]
                (do* ,(cdr vars) ,@body)))
-           (,'recur (next ,iter)))))))
+           (,'recur (next ,iter))))))) ; }}}
 
-(defun map (fun iter)
-  "Return an iterator that applies FUN to every non-nil value produced
-   by ITER."
-  { :tag "iterator.map" :fun fun :backing iter })
+;;; }}}
 
-(defmethod (next iterator.map) (x)
-  (destructuring-bind [{ :fun ?fun
-                         :backing ?iter }
-                       x]
-    (let* [((val iter) (next iter))]
-      (if (not iter)
-        nil
-        (values-list (fun val) { :tag "iterator.map" :fun fun :backing iter })))))
+;;; Catamorphisms (folds) {{{
 
-(defun to-list (iter)
+(defun to-list (iter) ; {{{
   "Collect all non-nil values produced by ITER into a list."
   (let* [(out '())]
     (do* [(v iter)]
       (push-cdr! out v))
     out))
+
+(defun collapse (iter)
+  "Collapse is an alias for [[to-list]]."
+  (to-list iter)) ; }}}
+
+(defun fold (f z iter) ; {{{
+  "Accumulate all values produced by ITER using the function F, starting
+   from the value Z.
+
+   ```cl
+   > (fold + 0 (range :from 1 :to 10))
+   out = 55
+   ```"
+  ; Copy the iterator into the correct place when Z is not given
+  (loop [(val z)
+         (iter iter)]
+    [(= iter nil) val]
+    (let* [((newv newi)
+            (next iter))]
+      (recur (if newv
+               (f val newv)
+               val)
+             newi)))) ; }}}
+
+;;; }}}
+
+; vim: fdm=marker
