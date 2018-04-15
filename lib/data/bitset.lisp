086720338e87fe5167b8da0258b2a7275972fb27
Scitoshi Nakayobro <matthewtpeterson1@gmail.com>
2018-04-15 13:34:06 -0500

    Implement a bitset (#22)

---
+++ lib/data/bitset.lisp
@@ -0,0 +1,356 @@
+"This module implements bit sets"
+
+(import core/base (set-idx!))
+(import data/struct ())
+(import lua/math (ceil floor min max))
+(import lua/bit32 ())
+(import lua/string (format))
+
+;; This is questionable but 32 bits seems like a safe assumption
+(define bits-per-int :hidden 32)
+
+(defun bit->element (bit)
+  :hidden
+  (floor (/ bit bits-per-int)))
+
+(defun bit->element-index (bit)
+  :hidden
+  (mod bit bits-per-int))
+
+(defun ensure-bit-exists! (bs bit)
+  :hidden
+  (let* [(data (bitset-data bs))
+         (need (- (+ (bit->element bit) 1) (n data)))]
+    (when (> need 0)
+      (for i 1 need 1
+        (push! data 0)))))
+
+(defstruct (bitset make-bitset bitset?)
+  "Creates a new, empty bitset"
+  (fields
+    (immutable (hide data)))
+  (constructor new
+    (lambda (other)
+      (if (bitset? other)
+        (new (map id (bitset-data other)))
+        (new '())))))
+
+(defmethod (eq? bitset bitset) (x y)
+  (let* [(dx (bitset-data x))
+         (dy (bitset-data y))
+         (ldx (n dx))
+         (ldy (n dy))]
+    (if (= ldx ldy)
+      (with (result true)
+        (for i 1 ldx 1
+          (when (/= (nth dx i) (nth dy i))
+            (set! result false)))
+        result)
+      false)))
+
+(defmethod (pretty bitset) (bs)
+  (.. "«bitset: " (concat (map (lambda (x) (format "%08x" x)) (bitset-data bs)) " ") "»"))
+
+(defun get-bit (bs bit)
+  "Returns the value of the bit in the bitset BS with the specified index BIT
+
+   ### Example:
+   ```cl
+   > (define bs (make-bitset))
+   out = «bitset: »
+   > (set-bit! bs 5)
+   out = nil
+   > (get-bit bs 5)
+   out = true
+   ```"
+  (assert-type! bs bitset)
+  (let* [(data (bitset-data bs))
+         (eindex (+ (bit->element bit) 1))]
+    (if (<= eindex (n data))
+      (bit-test (nth data eindex) (shl 1 (bit->element-index bit)))
+      false)))
+
+(defun set-bit! (bs bit)
+  "Sets the bit in the bitset BS with the specified index BIT
+
+   ### Example:
+   ```cl
+   > (define bs (make-bitset))
+   out = «bitset: »
+   > (set-bit! bs 5)
+   out = nil
+   > bs
+   out = «bitset: 00000020»
+   ```"
+  (assert-type! bs bitset)
+  (ensure-bit-exists! bs bit)
+  (let* [(data (bitset-data bs))
+        (eindex (+ (bit->element bit) 1))
+        (orig (nth data eindex))]
+    (set-idx! data eindex (bit-or orig (shl 1 (bit->element-index bit))))))
+
+(defun clear-bit! (bs bit)
+  "Clears the bit in the bitset BS with the specified index BIT
+
+   ```cl
+   > (define bs (make-bitset))
+   out = «bitset: »
+   > (set-bit! bs 5)
+   out = nil
+   > bs
+   out = «bitset: 00000020»
+   > (clear-bit! bs 5)
+   out = nil
+   > bs
+   out = «bitset: 00000000»
+   ```"
+  (assert-type! bs bitset)
+  (let* [(data (bitset-data bs))
+         (eindex (+ (bit->element bit) 1))]
+    (when (<= eindex (n data))
+      (set-idx! data eindex (bit-and (nth data eindex) (bit-not (shl 1 (bit->element-index bit))))))))
+
+(defun set-bit-value! (bs bit value)
+  "Sets the value of the bit in the bitset BS with the specified index BIT to VALUE
+
+   ```cl
+   > (define bs (make-bitset))
+   out = «bitset: »
+   > (set-bit-value! bs 2 true)
+   out = nil
+   > bs
+   out = «bitset: 00000004»
+   > (set-bit-value! bs 2 false)
+   out = nil
+   > bs
+   out = «bitset: 00000000»"
+  (if value
+    (set-bit! bs bit)
+    (clear-bit! bs bit)))
+
+(defun flip-bit! (bs bit)
+  "Inverts the value of the bit in the bitset BS with the specified index BIT
+
+   ### Example:
+   ```cl
+   > (define bs (make-bitset))
+   out = «bitset: »
+   > (flip-bit! bs 2)
+   out = nil
+   > bs
+   out = «bitset: 00000004»
+   > (flip-bit! bs 2)
+   out = nil
+   > bs
+   out = «bitset: 00000000»
+   ```"
+  (assert-type! bs bitset)
+  (ensure-bit-exists! bs bit)
+  (let* [(data (bitset-data bs))
+         (eindex (+ (bit->element bit) 1))
+         (orig (nth data eindex))]
+    (set-idx! data eindex (bit-xor orig (shl 1 (bit->element-index bit))))))
+
+(defun next-set-bit (bs start)
+  "Finds the next set bit in the bitset BS after the index START. If no set bit is found, -1 is returned
+
+   ### Example:
+   ```cl
+   > (define bs (make-bitset))
+   out = «bitset: »
+   > (set-bit! bs 5)
+   out = nil
+   > (next-set-bit bs 2)
+   out = 5
+   ```"
+  (assert-type! bs bitset)
+  (let* [(data (bitset-data bs))
+         (len (n data))
+         (sindex (bit->element start))
+         (eindex sindex)
+         (result -1)]
+    (demand (<= sindex len))
+    (while (and (= result -1) (< eindex len))
+      (for i 0 (- bits-per-int 1) 1
+        (with (j (+ (* eindex bits-per-int) i))
+          (when (and (= result -1) (get-bit bs j) (> j start))
+            (set! result j))))
+      (inc! eindex))
+    result))
+
+(defun next-clear-bit (bs start)
+  "Finds the next clear bit in the bitset BS after the index START. If no clear bit is found, -1 is returned
+
+   ### Example:
+   ```cl
+   > (define bs (make-bitset))
+   out = «bitset: »
+   > (set-bit! bs 0)
+   out = nil
+   > (set-bit! bs 1)
+   out = nil
+   > (next-clear-bit bs 0)
+   out = 2
+   ```"
+  (assert-type! bs bitset)
+  (let* [(data (bitset-data bs))
+         (len (n data))
+         (sindex (bit->element start))
+         (eindex sindex)
+         (result -1)]
+    (demand (<= sindex len))
+    (while (and (= result -1) (< eindex len))
+      (for i 0 (- bits-per-int 1) 1
+        (with (j (+ (* eindex bits-per-int) i))
+          (when (and (= result -1) (not (get-bit bs j)) (> j start))
+            (set! result j))))
+      (inc! eindex))
+    result))
+
+(defun cardinality (bs)
+  "Returns the number of set bits in the bitset BS
+
+   ### Example:
+   ```cl
+   > (define bs (make-bitset))
+   out = «bitset: »
+   > (set-bit! bs 1)
+   out = nil
+   > (set-bit! bs 4)
+   out = nil
+   > (cardinality bs)
+   out = 2
+   ```"
+  (assert-type! bs bitset)
+  (let* [(data (bitset-data bs))
+         (count 0)]
+    (for-each elem data
+      (for i 0 (- bits-per-int 1) 1
+        (when (bit-test elem (shl 1 i))
+          (inc! count))))
+    count))
+
+(defun intersects (x y)
+  "Tests if two bitsets share any of the same set bits
+
+   ### Example:
+   ```cl
+   > (define a (make-bitset))
+   out = «bitset: »
+   > (define b (make-bitset))
+   out = «bitset: »
+   > (set-bit! a 2)
+   out = nil
+   > (set-bit! b 2)
+   out = nil
+   > (intersects a b)
+   out = true
+   ```"
+  (assert-type! x bitset)
+  (assert-type! y bitset)
+  (let* [(dx (bitset-data x))
+         (dy (bitset-data y))
+         (len (min (n dx) (n dy)))
+         (result false)]
+    (for i 1 len 1
+      (when (bit-test (nth dx i) (nth dy i))
+        (set! result true)))
+    result))
+
+(defun bitsets-and (x y)
+  "Performs a logical AND between two bitsets and returns the result as a new bitset
+
+   ### Example:
+   ```cl
+   > (define a (make-bitset))
+   out = «bitset: »
+   > (define b (make-bitset))
+   out = «bitset: »
+   > (set-bit! a 2)
+   out = nil
+   > (set-bit! b 2)
+   out = nil
+   > (bitsets-and a b)
+   out = «bitset: 00000004»
+   ```"
+  (assert-type! x bitset)
+  (assert-type! y bitset)
+  (let* [(result '())
+        (dx (bitset-data x))
+        (dy (bitset-data y))
+        (len (min (n dx) (n dy)))]
+    (for i 1 len 1
+      (push! result (bit-and (nth dx i) (nth dy i))))
+    (with (rbs (make-bitset))
+      (.<! rbs :data result)
+      rbs)))
+
+(defun bitsets-or (x y)
+  "Performs a logical OR between two bitsets and returns the result as a new bitset
+
+   ### Example:
+   ```cl
+   > (define a (make-bitset))
+   out = «bitset: »
+   > (define b (make-bitset))
+   out = «bitset: »
+   > (set-bit! a 0)
+   out = nil
+   > (set-bit! b 1)
+   out = nil
+   > (bitsets-or a b)
+   out = «bitset: 00000003»
+   ```"
+  (assert-type! x bitset)
+  (assert-type! y bitset)
+  (let* [(result '())
+         (dx (bitset-data x))
+         (dy (bitset-data y))
+         (xlen (n dx))
+         (ylen (n dy))
+         (len (max xlen ylen))]
+    (for i 1 len 1
+      (if (and (<= i xlen) (<= i ylen))
+        (push! result (bit-or (nth dx i) (nth dy i)))
+        (if (<= i xlen)
+          (push! result (nth dx i))
+          (push! result (nth dy i)))))
+    (with (rbs (make-bitset))
+      (.<! rbs :data result)
+      rbs)))
+
+(defun bitsets-xor (x y)
+  "Performs a logical XOR between two bitsets and returns the result as a new bitset
+
+   ### Example:
+   ```cl
+   > (define a (make-bitset))
+   out = «bitset: »
+   > (define b (make-bitset))
+   out = «bitset: »
+   > (set-bit! a 0)
+   out = nil
+   > (set-bit! a 1)
+   out = nil
+   > (set-bit! b 1)
+   out = nil
+   > (bitsets-xor a b)
+   out = «bitset: 00000001»
+   ```"
+  (assert-type! x bitset)
+  (assert-type! y bitset)
+  (let* [(result '())
+         (dx (bitset-data x))
+         (dy (bitset-data y))
+         (xlen (n dx))
+         (ylen (n dy))
+         (len (max xlen ylen))]
+    (for i 1 len 1
+      (if (and (<= i xlen) (<= i ylen))
+        (push! result (bit-xor (nth dx i) (nth dy i)))
+        (if (<= i xlen)
+          (push! result (nth dx i))
+          (push! result (nth dy i)))))
+    (with (rbs (make-bitset))
+      (.<! rbs :data result)
+      rbs)))
---
+++ tests/lib/data/bitset.lisp
@@ -0,0 +1,171 @@
+(import test ())
+
+(import data/bitset ())
+
+(describe "A bitset"
+  (it "looks like a bitset"
+    (affirm (= (type (make-bitset)) "bitset")))
+
+  (it "can be checked for equality"
+    (let [(a (make-bitset))
+          (b (make-bitset))]
+      (set-bit! a 3)
+      (set-bit! a 7)
+      (set-bit! b 3)
+      (set-bit! b 7)
+      (affirm (eq? a b)))
+    (let [(a (make-bitset))
+          (b (make-bitset))]
+      (set-bit! a 5)
+      (set-bit! a 7)
+      (set-bit! b 6)
+      (set-bit! b 7)
+      (affirm (not (eq? a b)))))
+
+  (it "can be pretty printed"
+    (with (bs (make-bitset))
+      (affirm (= (pretty bs) "«bitset: »"))
+      (set-bit! bs 0)
+      (affirm (= (pretty bs) "«bitset: 00000001»"))
+      (set-bit! bs 2)
+      (affirm (= (pretty bs) "«bitset: 00000005»"))
+      (set-bit! bs 33)
+      (affirm (= (pretty bs) "«bitset: 00000005 00000002»"))))
+
+  (it "has a copying constructor"
+    (with (a (make-bitset))
+      (set-bit! a 4)
+      (with (b (make-bitset a))
+        (affirm (not (= a b)))
+        (affirm (get-bit b 4)))))
+
+  (it "can have bits set and cleared"
+    (with (bs (make-bitset))
+      (set-bit! bs 2)
+      (set-bit! bs 32)
+      (set-bit! bs 33)
+      (affirm (get-bit bs 2))
+      (affirm (get-bit bs 32))
+      (affirm (get-bit bs 33))
+      (clear-bit! bs 2)
+      (clear-bit! bs 32)
+      (clear-bit! bs 33)
+      (affirm (not (get-bit bs 2)))
+      (affirm (not (get-bit bs 32)))
+      (affirm (not (get-bit bs 33)))
+      (set-bit-value! bs 4 true)
+      (affirm (get-bit bs 4))
+      (set-bit-value! bs 4 false)
+      (affirm (not (get-bit bs 4)))))
+
+  (it "can have bits flipped"
+    (with (bs (make-bitset))
+      (flip-bit! bs 2)
+      (flip-bit! bs 33)
+      (affirm (get-bit bs 2))
+      (affirm (get-bit bs 33))
+      (flip-bit! bs 2)
+      (flip-bit! bs 33)
+      (affirm (not (get-bit bs 2)))
+      (affirm (not (get-bit bs 33)))))
+
+  (it "can find the next set bit"
+    (with (bs (make-bitset))
+      (set-bit! bs 5)
+      (set-bit! bs 15)
+      (set-bit! bs 33)
+      (affirm (= (next-set-bit bs 2) 5))
+      (affirm (= (next-set-bit bs 6) 15))
+      (affirm (= (next-set-bit bs 30) 33))
+      (affirm (= (next-set-bit bs 33) -1))))
+
+  (it "can find the next clear bit"
+    (with (bs (make-bitset))
+      (set-bit! bs 0)
+      (set-bit! bs 1)
+      (set-bit! bs 2)
+      (set-bit! bs 3)
+      (set-bit! bs 5)
+      (set-bit! bs 7)
+      (affirm (= (next-clear-bit bs 2) 4))
+      (affirm (= (next-clear-bit bs 5) 6))
+      (affirm (= (next-clear-bit bs 7) 8))))
+
+  (it "can count the number of set bits"
+    (with (bs (make-bitset))
+      (set-bit! bs 2)
+      (set-bit! bs 3)
+      (set-bit! bs 5)
+      (affirm (= (cardinality bs) 3))))
+
+  (it "can test if two bitsets intersect"
+    (let [(a (make-bitset))
+          (b (make-bitset))]
+      (set-bit! a 2)
+      (set-bit! a 5)
+      (set-bit! b 3)
+      (set-bit! b 6)
+      (affirm (not (intersects a b))))
+    (let [(a (make-bitset))
+          (b (make-bitset))]
+      (set-bit! a 2)
+      (set-bit! a 5)
+      (set-bit! b 5)
+      (set-bit! b 6)
+      (affirm (intersects a b))))
+
+  (it "can perform a logical AND between two bitsets"
+    (let [(a (make-bitset))
+          (b (make-bitset))]
+      (set-bit! a 1)
+      (set-bit! a 3)
+      (set-bit! a 6)
+      (set-bit! b 3)
+      (set-bit! b 6)
+      (set-bit! b 9)
+      (with (c (bitsets-and a b))
+        (affirm (not (get-bit c 1)))
+        (affirm (not (get-bit c 2)))
+        (affirm (get-bit c 3))
+        (affirm (not (get-bit c 4)))
+        (affirm (not (get-bit c 5)))
+        (affirm (get-bit c 6))
+        (affirm (not (get-bit c 9))))))
+
+  (it "can perform a logical OR between two bitsets"
+    (let [(a (make-bitset))
+          (b (make-bitset))]
+      (set-bit! a 2)
+      (set-bit! a 5)
+      (set-bit! a 15)
+      (set-bit! a 33)
+      (set-bit! b 5)
+      (set-bit! b 40)
+      (set-bit! b 66)
+      (with (c (bitsets-or a b))
+        (affirm (get-bit c 2))
+        (affirm (get-bit c 5))
+        (affirm (get-bit c 15))
+        (affirm (get-bit c 33))
+        (affirm (get-bit c 40))
+        (affirm (get-bit c 66)))))
+
+  (it "can perform a logical XOR between two bitsets"
+    (let [(a (make-bitset))
+          (b (make-bitset))]
+      (set-bit! a 2)
+      (set-bit! a 7)
+      (set-bit! a 12)
+      (set-bit! a 23)
+      (set-bit! b 5)
+      (set-bit! b 7)
+      (set-bit! b 16)
+      (set-bit! b 23)
+      (with (c (bitsets-xor a b))
+        (affirm (not (get-bit c 0)))
+        (affirm (get-bit c 2))
+        (affirm (get-bit c 5))
+        (affirm (not (get-bit c 7)))
+        (affirm (get-bit c 12))
+        (affirm (get-bit c 16))
+        (affirm (not (get-bit c 23)))))))
